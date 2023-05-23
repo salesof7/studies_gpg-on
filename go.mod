112db51ddc0081729312f18ab0891fcfedd83bdc
salesof7 <jpsaless2002@gmail.com>
2023-05-23 17:56:50 -0300

    chore: add something curious

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/salesof7/studies_go-curiosity
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Don't be curious enough to run: 'docker run salesof7/studies_go-curiosity'!")
+}
